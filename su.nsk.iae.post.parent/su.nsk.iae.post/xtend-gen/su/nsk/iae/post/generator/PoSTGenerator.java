3aa6c7d6edc76be2139e5cd0c9ba09c9b9250c5f
Darlitch <errorguy@mail.ru>
2026-03-06 14:38:27 +0700

    .

---
+++ su.nsk.iae.post.parent/su.nsk.iae.post/xtend-gen/su/nsk/iae/post/generator/PoSTGenerator.java
@@ -0,0 +1,56 @@
+package su.nsk.iae.post.generator;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.generator.AbstractGenerator;
+import org.eclipse.xtext.generator.IFileSystemAccess2;
+import org.eclipse.xtext.generator.IGeneratorContext;
+import org.eclipse.xtext.xbase.lib.Exceptions;
+
+@SuppressWarnings("all")
+public class PoSTGenerator extends AbstractGenerator {
+  private static final String EXTENSION_ID = "su.nsk.iae.post.post_extension";
+
+  private static final List<IPoSTGenerator> generators = new ArrayList<IPoSTGenerator>();
+
+  public static void initGenerators() {
+    try {
+      final IConfigurationElement[] configuration = Platform.getExtensionRegistry().getConfigurationElementsFor(PoSTGenerator.EXTENSION_ID);
+      for (final IConfigurationElement el : configuration) {
+        {
+          final Object obj = el.createExecutableExtension("class");
+          if ((obj instanceof IPoSTGenerator)) {
+            PoSTGenerator.generators.add(((IPoSTGenerator)obj));
+          }
+        }
+      }
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+
+  @Override
+  public void beforeGenerate(final Resource resource, final IFileSystemAccess2 fsa, final IGeneratorContext context) {
+    throw new Error("Unresolved compilation problems:"
+      + "\nPyGenerator cannot be resolved."
+      + "\nPyGenerator cannot be resolved."
+      + "\ndoGenerate cannot be resolved");
+  }
+
+  @Override
+  public void doGenerate(final Resource resource, final IFileSystemAccess2 fsa, final IGeneratorContext context) {
+    for (final IPoSTGenerator g : PoSTGenerator.generators) {
+      g.doGenerate(resource, fsa, context);
+    }
+  }
+
+  @Override
+  public void afterGenerate(final Resource resource, final IFileSystemAccess2 fsa, final IGeneratorContext context) {
+    for (final IPoSTGenerator g : PoSTGenerator.generators) {
+      g.afterGenerate(resource, fsa, context);
+    }
+  }
+}
